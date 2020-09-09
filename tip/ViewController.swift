6cdcde5dc1ee904cb881c827807071974e80160d
android-47 <jx.garcia@outlook.com>
2020-09-09 02:39:51 -0700

    Add spliting bill feature

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var numberofPeopleLabel: UILabel!
+    @IBOutlet weak var splitSlider: UISlider!
     @IBOutlet weak var totalLabel: UILabel!
-    
-    
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.becomeFirstResponder()
     }
     
     @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func numberOfPeopleValueChanged(sender: Any) {
+        numberofPeopleLabel.text = String(Int(splitSlider.value))
     }
     
     @IBAction func calculateTip(_ sender: Any) {
@@ -31,14 +36,19 @@ class ViewController: UIViewController {
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.20]
         
-        // calculate tip and total
+        // Determine number of people to split bill
+        let numberOfPeople: Int = Int(splitSlider.value)
+
+        // calculate tip, total bill, and total per person
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
-        
-        // update the tip and total labels
-        tipPercentageLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let totalPerPerson = total / Double(numberOfPeople)
 
+        // update the tip and total labels
+        tipPercentageLabel.text = String(format: "$%0.2f", tip)
+        totalPerPersonLabel.text = String(format: "$%0.2f", totalPerPerson)
+        totalLabel.text = String(format: "$%0.2f", total)
+        
     }
     
 }
